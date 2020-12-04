af33caa6d5b8394b51d1910893134359ac68fcca
Surya Wiguna <suryawigunaa@gmail.com>
2020-12-04 20:21:08 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import 'assets/scss/style.scss';
+import LandingPage from 'pages/LandingPage';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
     </div>
   );
 }
