a3a38e05216b33a74dbd9d3c6d186f3cbd9111c4
Surya Wiguna <suryawigunaa@gmail.com>
2020-12-08 21:57:09 +0800

    section most picked

---
+++ src/parts/MostPicked.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Button from "elements/Button";
+
+export default function MostPicked(props) {
+  return (
+    <section className="container" ref={props.refMostPicked}>
+      <h4 className="mb-3">Most Picked</h4>
+      <div className="container-grid">
+        {props.data.map((item, index) => {
+          return (
+            <div
+              key={`mostpicked-${index}`}
+              className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
+            >
+              <div className="card card-featured">
+                <div className="tag">
+                  <strong>${item.price}</strong>
+                  <span className="font-weight-light"> / {item.unit}</span>
+                </div>
+                <figure className="img-wrapper">
+                  <img
+                    src={item.imageUrl}
+                    alt={item.name}
+                    className="img-cover"
+                  />
+                </figure>
+                <div className="meta-wrapper">
+                  <Button
+                    type="link"
+                    className="stretched-link d-block text-white"
+                    href={`/properties/${item._id}`}
+                  >
+                    <h5 className="font-weight-bold text-decoration-none">{item.name}</h5>
+                  </Button>
+                  <span>
+                    {item.city}, {item.country}
+                  </span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
