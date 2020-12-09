4bdcf18a9c7159727db1da2e3356821571124a12
Surya Wiguna <suryawigunaa@gmail.com>
2020-12-09 12:43:50 +0800

    polishing formatted number function

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -1,4 +1,5 @@
-export default (number) => {
+const formatNumber = (number) => {
   const formattedNumber = new Intl.NumberFormat("id-ID");
   return formattedNumber.format(number);
 }
+export default formatNumber;
